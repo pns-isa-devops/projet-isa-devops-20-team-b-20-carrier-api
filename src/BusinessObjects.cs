fe9c0764d3c034b29543f31b552aafbe03872e61
AlexandreLon <alexandre.longordo@etu.unice.fr>
2020-04-24 11:40:54 +0200

    pns-isa-devops/projet-isa-devops-20-team-b-20#100 pns-isa-devops/projet-isa-devops-20-team-b-20#101 Send all information of parcel

--- src/BusinessObjects.cs
+++ src/BusinessObjects.cs
@@ -9,14 +9,20 @@ namespace Carrier.Data
     {
         [DataMember]
         public string ParcelNumber { get; set; }
+        [DataMember]
         public string Address { get; set; }
+        [DataMember]
         public string Carrier { get; set; }
+        [DataMember]
         public string CustomerName { get; set; }
 
         Parcel(){}
 
         public Parcel(string id, string Address, string Carrier, string CustomerName){
             this.ParcelNumber = id;
+            this.Address = Address;
+            this.Carrier = Carrier;
+            this.CustomerName = CustomerName;
         }
 
         override public string ToString()
