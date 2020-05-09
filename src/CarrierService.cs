231618248d507d7cd34b3bd870de53bb064f400e
Jason Haenlin <jason.haenlin@etu.univ-cotedazur.fr>
2020-05-09 23:32:13 +0200

    add more parcels  pns-isa-devops/projet-isa-devops-20-team-b-20#158

--- src/CarrierService.cs
+++ src/CarrierService.cs
@@ -17,13 +17,29 @@ namespace Carrier.Service
     {
         public List<Parcel> parcels = new List<Parcel>();
 
-        private void MOCK_DATA(){
+        private void MOCK_DATA()
+        {
             parcels.Add(new Parcel("123456789A", "Instant travel", "Colissimo", "Martine"));
             parcels.Add(new Parcel("123456789B", "5 rue Vert", "Colissimo", "Francis"));
             parcels.Add(new Parcel("123456789C", "10 rue Orange", "Colissimo", "Jean"));
             parcels.Add(new Parcel("123456789D", "900 rue Rouge", "Colissimo", "Patrick"));
             parcels.Add(new Parcel("123456789E", "900 rue Rouge", "Colissimo", "Florence"));
             parcels.Add(new Parcel("123456789F", "5 rue Vert", "Colissimo", "Lucile"));
+            parcels.Add(new Parcel("123456789G", "Instant travel", "Colissimo", "Martine"));
+            parcels.Add(new Parcel("123456789H", "5 rue Vert", "Colissimo", "Francis"));
+            parcels.Add(new Parcel("123456789I", "10 rue Orange", "Colissimo", "Jean"));
+            parcels.Add(new Parcel("123456789J", "900 rue Rouge", "Colissimo", "Patrick"));
+            parcels.Add(new Parcel("123456789K", "900 rue Rouge", "Colissimo", "Florence"));
+            parcels.Add(new Parcel("123456789L", "5 rue Vert", "Colissimo", "Lucile"));
+            parcels.Add(new Parcel("123456789M", "Instant travel", "Colissimo", "Martine"));
+            parcels.Add(new Parcel("123456789N", "5 rue Vert", "Colissimo", "Francis"));
+            parcels.Add(new Parcel("123456789O", "10 rue Orange", "Colissimo", "Jean"));
+            parcels.Add(new Parcel("123456789P", "900 rue Rouge", "Colissimo", "Patrick"));
+            parcels.Add(new Parcel("123456789Q", "900 rue Rouge", "Colissimo", "Florence"));
+            parcels.Add(new Parcel("123456789R", "5 rue Vert", "Colissimo", "Lucile"));
+            parcels.Add(new Parcel("123456789S", "10 rue Orange", "Colissimo", "Jean"));
+            parcels.Add(new Parcel("123456789T", "900 rue Rouge", "Colissimo", "Patrick"));
+            parcels.Add(new Parcel("123456789U", "900 rue Rouge", "Colissimo", "Florence"));
         }
 
         public CarrierService()
@@ -36,13 +52,16 @@ namespace Carrier.Service
             return "route not found";
         }
 
-        public List<Parcel> getParcels(){
+        public List<Parcel> getParcels()
+        {
             return this.parcels;
         }
 
-        public Parcel getParcel(string id){
-            foreach(Parcel p in this.parcels){
-                if(string.Equals(p.ParcelNumber, id))
+        public Parcel getParcel(string id)
+        {
+            foreach (Parcel p in this.parcels)
+            {
+                if (string.Equals(p.ParcelNumber, id))
                     return p;
             }
             return null;
